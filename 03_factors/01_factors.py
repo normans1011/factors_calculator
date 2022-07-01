0a0629a5e73193b26e3158c78e6ff2f84057aa48
normans1011 <101604933+normans1011@users.noreply.github.com>
2022-07-01 15:00:11 +1200

    Add files via upload

--- 03_factors/01_factors.py
+++ 03_factors/01_factors.py
@@ -17,7 +17,12 @@ def statement_generator(text, decoration):
     return ""
 # Displays instructions / information
 
+
+
+
 # Checks input is a number more than a given value
+
+
 
 # Gets factors, returns a sorted list
 
@@ -29,9 +34,33 @@ def statement_generator(text, decoration):
 statement_generator("Factors Calculator","-")
 
 # Display instructions if user has not used the program before
-
+first_time = input("Press <enter> to see instructions, or any key to continue")
 
 # Loop to allow multiple calculations per session
+keep_going = ""
+while keep_going == "":
+
+    comment = ""
+
+    # ask user for number to be factored...
+    var_to_factor = num_check("Number? ")
+
+    if var_to_factor != 1:
+        factors_list = get_factors(var_to_factor)
+
+    else:
+        factor_list = ""
+        comment = "One is UNITY! It has only one factor. Itself."
+
+
+    # comments for squares / primes
+    if len(factor_list) == 2:
+        comment = "{} is a prime number.".format(var_to_factor)
+
+    elif len(factor_list) % 2 == 1:
+        comment = "{} is a perfect square".format(var_to_factor)
+
+
 
 
 
